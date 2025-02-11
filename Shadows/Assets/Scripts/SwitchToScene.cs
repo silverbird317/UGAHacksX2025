5b3a1522dcae6bd8c1d5d687a5060637923583a5
silverbird317 <dfadga863@gmail.com>
2025-02-11 00:52:11 -0500

    add reset level function

--- Shadows/Assets/Scripts/SwitchToScene.cs
+++ Shadows/Assets/Scripts/SwitchToScene.cs
@@ -4,42 +4,58 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SwitchToScene : MonoBehaviour
-{
+public class SwitchToScene : MonoBehaviour {
     string filenamePath = "Assets/Contours/RetrySceneName.txt";
     string sceneName = "";
-    public void SwitchScene(string sceneName)
-    {
+
+    /*
+     * switch scene to the a specific scene
+     * 
+     * parameters: string sceneName
+     * returns: n/a
+     */
+    public void SwitchScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     } // SwitchScene
 
-    public void RetryLevel()
-    {
-        // get filename from txt file
-        using (StreamReader read = new StreamReader(filenamePath))
-        {
+    /*
+     * return from 2d level to the corresponding 3d level
+     * 
+     * parameters: n/a
+     * returns: n/a
+     */
+    public void RetryLevel() {
+        // get filename from RetrySceneName.txt file
+        using (StreamReader read = new StreamReader(filenamePath)) {
             string line;
-            while ((line = read.ReadLine()) != null)
-            {
+            // loop through txt file
+            while ((line = read.ReadLine()) != null) {
                 sceneName = line;
             } // while
-
         } // using
         SceneManager.LoadScene(sceneName);
     } // RetryLevel
 
+    /*
+     * reset the 3d level objects to their starting positions
+     * 
+     * parameters: n/a
+     * returns: n/a
+     */
+    public void ResetLevel() {
+        // get current scene name and load scene by name
+        string sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+    } // ResetLevel
+
+    /*
+     * switch to scene for next level
+     * if last level, go to main menu scene
+     * 
+     * parameters: n/a
+     * returns: n/a
+     */
     public void Next() {
-// get filename from txt file
-        using (StreamReader read = new StreamReader(filenamePath))
-        {
-            string line;
-            while ((line = read.ReadLine()) != null)
-            {
-                sceneName = line;
-            } // while
-
-        } // using
-
         if (sceneName.Equals("TutorialLevelOne")) {
             SceneManager.LoadScene("TutorialLevelTwo");
         } else if (sceneName.Equals("TutorialLevelTwo")) {
@@ -52,10 +68,8 @@ public class SwitchToScene : MonoBehaviour
             SceneManager.LoadScene("MainSceneFour");
         } else if (sceneName.Equals("MainSceneFour")) {
             SceneManager.LoadScene("MainSceneFive");
-        } /*else if (sceneName.Equals("MainSceneFive")) {
-            SceneManager.LoadScene("MainSceneSix");
-        } */else {
+        } else {
              SceneManager.LoadScene("MainMenuScene");
-        }
-    }
-}
+        } // if-else
+    } // Next
+} // SwitchToScene
